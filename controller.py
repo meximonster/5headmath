24210ec998a918978be0b002ee67b315e26c222e
meximonster <tiltaki@gmail.com>
2021-04-14 02:04:53 +0300

    change host,port

--- controller.py
+++ controller.py
@@ -27,4 +27,4 @@ def index():
         return render_template("view_input.html", form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="stefanos-5head.herokuapp.com", port=8000, debug=True)
