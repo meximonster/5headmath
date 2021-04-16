9ed5795d0f7e5765b5fe619bbd16d56c6380aaf3
meximonster <tiltaki@gmail.com>
2021-04-16 20:48:33 +0300

    implement rounding of sum values

--- compute.py
+++ compute.py
@@ -2,7 +2,24 @@ import constants
 import math
 
 def compute_upper(ar,su,sl):
-    return round(ar - su - 2*constants.Uupper[sl], 1)
+    slr = switch(sl)
+    return round(ar - su - 2*constants.Uupper[slr], 1)
 
 def compute_lower(ar,sl):
-    return round(ar - sl - 2*constants.Ulower[sl], 1)
+    slr = switch(sl)
+    return round(ar - sl - 2*constants.Ulower[slr], 1)
+
+def switch(s):
+    if len(str(s).split('.')[1]) <= 2:
+        format(s, '.2f')
+    else:
+        s = round(s, 2)
+    print(s)
+    num = float(str(s).split('.')[0])
+    dec = float(str(s).split('.')[1])
+    if 0 <= dec < 25:
+        return num
+    elif 25 <= dec < 75:
+        return float(str(s).split('.')[0]+".5")
+    else:
+        return num+1
