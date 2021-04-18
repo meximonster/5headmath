f65f6692970c9cfb3ee4702d20bd2b9eddab353e
meximonster <tiltaki@gmail.com>
2021-04-18 14:40:52 +0300

    test validate function works

--- app.py
+++ app.py
@@ -7,16 +7,27 @@ sumMessage = "Sum should always be between 19.5 and 29."
 
 # Model
 class InputForm(Form):
-    al_upper = FloatField(label="Arch length :", validators=[validators.InputRequired()])
-    s_upper = FloatField(label="Sum of incisor widths :", validators=[validators.InputRequired(), validators.NumberRange(min=19.5, max=29, message=sumMessage)])
-    al_lower = FloatField(label="Arch length :", validators=[validators.InputRequired()])
+    al_lower = FloatField(label="Arch length :", validators=[validators.Optional()])
     s_lower = FloatField(label="Sum of incisor widths :", validators=[validators.InputRequired(), validators.NumberRange(min=19.5, max=29, message=sumMessage)])
+    al_upper = FloatField(label="Arch length :", validators=[validators.Optional()])
+    s_upper = FloatField(label="Sum of incisor widths :", validators=[validators.Optional(), validators.NumberRange(min=19.5, max=29, message=sumMessage)])
+
+    def validate(self, al_lower, al_upper):
+        self.al_lower.errors = []
+        self.al_upper.errors = []
+        if self.al_lower.data == 25 and self.al_upper.data == 25:
+            self.al_lower.errors.append("omg")
+            self.al_upper.errors.append("lol")
+            return False
+        else:
+            return True
+
 
 # View
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputForm(request.form)
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST' and form.validate(form.al_lower, form.al_upper):
         al_l = form.al_lower.data
         s_l = form.s_lower.data
         al_u = form.al_upper.data
